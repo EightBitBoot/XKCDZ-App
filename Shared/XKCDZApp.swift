afaa6e7354c432aa324f3ae9684c09ced11bd095
Adin W-T <adinrwt@gmail.com>
2022-04-12 10:07:11 -0400

    Initial Commit

---
+++ Shared/XKCDZApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKCDZApp.swift
+//  Shared
+//
+//  Created by Adin on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct XKCDZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
